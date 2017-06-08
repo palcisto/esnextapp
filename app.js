2379bd256316cddd1a195b2394dbfe4e07697bab
Brian <brsweat@gmail.com>
2017-06-07 19:33:14 -0700

    yeah budday

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 const express = require('express')
+const swig = require('swig')
 const app = express()
 
+app.engine('html', swig.renderFile)
+app.set('view engine', 'html')
+app.set('views', __dirname + '/views')
+app.use(express.static('build'))
+
 app.get('/', function (req, res) {
-  res.send('Hello World!')
+  res.render('index.html', { title: 'Hey Pat', message: 'Hope you like Swig as much as I like loam. Also, check the console to see the WebPack\'d js.' })
 })
 
 app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
+  console.log('App listening on port 3000!')
 })
